fc8b5d058046ba2c6c6d395ac7e17a8167aabb52
joapedro12345 <104033078+joapedro12345@users.noreply.github.com>
2023-10-24 23:11:43 -0300

    segundo exercicio

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -1,3 +1,7 @@
--- 1.
+-- 1 --
 CREATE TRIGGER novo_cliente AFTER INSERT ON Clientes
 	FOR EACH ROW INSERT INTO Auditoria VALUES("Um novo cliente foi adicionado");
+
+-- 2 --
+CREATE TRIGGER exclusao_cliente BEFORE DELETE ON Clientes
+	FOR EACH ROW INSERT INTO Auditoria VALUES("Houve uma tentativa de exclusão de um cliente");
