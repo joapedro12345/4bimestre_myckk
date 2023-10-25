e1f8f28c7a8ee19a76eb58683c9dce57e44b1262
joapedro12345 <104033078+joapedro12345@users.noreply.github.com>
2023-10-24 23:14:12 -0300

    quinto exercicio

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -39,3 +39,25 @@ CREATE TRIGGER impedir_insercao BEFORE UPDATE ON Clientes
 		END IF;
 	END //
 DELIMITER ;
+
+-- 5 --
+DELIMITER //
+CREATE TRIGGER pedido AFTER INSERT ON Pedidos
+	FOR EACH ROW
+	BEGIN
+		DECLARE produto_id INT;
+		DECLARE quantidade_pedida INT;
+		DECLARE estoque_atual INT;
+		
+		SET produto_id = NEW.produto_id;
+		SET quantidade_pedida = NEW.quantidade;
+		
+		SELECT estoque INTO estoque_atual FROM Produtos WHERE id = produto_id;
+
+		UPDATE Produtos SET estoque = estoque - quantidade_pedida WHERE id = produto_id;
+		
+		IF estoque_atual - quantidade_pedida < 5 THEN
+			INSERT INTO Auditoria VALUES("Estoque do produto " || produto_id || " está com menos de 5 unidades");
+		END IF;
+	END //
+DELIMITER ;
